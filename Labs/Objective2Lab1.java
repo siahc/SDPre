fd52cd2a94f2e60be877280422545ccf0950bc12
Josiah Candler <josiahcandler@Josiahs-MBP.lan>
2020-08-16 22:48:19 -0500

    use string variable

---
+++ Labs/Objective2Lab1.java
@@ -0,0 +1,10 @@
+public class Objective2Lab1 {
+  public static void main(String[] args) {
+    String hello = "Hello World";
+
+    /*
+      Your solution goes here
+    */
+    System.out.println(hello);
+  }
+}
