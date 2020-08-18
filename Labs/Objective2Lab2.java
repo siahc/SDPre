6db1e94b8456bc94e389c5446b3b87c4bc1bb387
Josiah Candler <josiahcandler@Josiahs-MBP.lan>
2020-08-18 11:58:36 -0500

    Complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,14 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Josiah Candler";
+    int age = 29;
+    String state = "California";
+
+    /*
+      Your solution goes here
+    */
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(state);
+  }
+}
