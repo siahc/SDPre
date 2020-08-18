8fd99990fd8f86d82ad91a5778fd6f113ee16824
Josiah Candler <josiahcandler@Josiahs-MBP.lan>
2020-08-18 14:50:36 -0500

    Completed Objective 3 Lab 1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,14 @@
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    double num1, num2, num3, average;
+
+    /*
+      Your solution goes here
+    */
+    num1 = 1;
+    num2 = 2;
+    num3 = 3;
+    average = (num1 + num2 + num3) /3;
+    System.out.println(average);
+  }
+}
