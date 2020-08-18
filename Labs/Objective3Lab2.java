8b7a7caa180017b9fb16b97146e3d564f7503ef1
Josiah Candler <josiahcandler@Josiahs-MBP.lan>
2020-08-18 16:06:12 -0500

    Completed Objective 3 Lab 2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,7 @@
+  public class Objective3Lab2 {
+    public static void main (String[] args) {
+      double num = Math.sqrt(8 * 8);
+      double result = Math.sqrt (9) + Math.sqrt(64);
+
+    }
+  }
