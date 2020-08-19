a32763695305071905ec8999b01aadf577ff699f
Josiah Candler <josiahcandler@Josiahs-MBP.lan>
2020-08-18 22:18:58 -0500

    Completed Objective 4 Lab 3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.time.Year;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+    int birthYear = 0;
+    int age = 0;
+    int currentYear = Year.now().getValue();
+
+    Scanner input = new Scanner(System.in);
+    System.out.println("How old are you?");
+    age = input.nextInt();
+    birthYear = (currentYear-age);
+
+    /*
+    Your solution goes here
+    */
+
+    System.out.println("You were born in " + birthYear);
+    input.close();
+  }
+}
