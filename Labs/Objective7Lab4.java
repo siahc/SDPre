41d9fa59938d371545aa127f388f08af7007d51c
Josiah Candler <josiahcandler@Josiahs-MBP.lan>
2020-08-19 21:48:08 -0500

    Completed Objective 7 Lab 4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,15 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+  int counter = 0;
+  int sum = 0;
+
+    while(counter < 20) {
+      /*
+      Your solution goes here
+      */
+      counter = counter+1;
+      sum = sum+counter;
+    }
+    System.out.println(sum);
+  }
+}
