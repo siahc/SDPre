e260980a86519c84acbb490f31c688bc1826221f
Josiah Candler <josiahcandler@Josiahs-MBP.lan>
2020-08-20 12:27:16 -0500

    completed objective 8 Lab 2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,16 @@
+public class Objective8Lab2 {
+    public static void main(String[] args) {
+    int sum = 0;
+
+for (int i=0; i <= 20; i++){
+
+      // while(counter< 20)
+
+        sum = sum+i;
+
+
+    }
+    System.out.println(sum);
+
+  }
+}
