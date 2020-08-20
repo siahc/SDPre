a3d3f9a1ea440aef181284783e40180b6bf781ef
Josiah Candler <josiahcandler@Josiahs-MBP.lan>
2020-08-20 14:15:13 -0500

    Completed Objective 8 Lab 3

---
+++ Labs/Objective8Lab3.java
@@ -0,0 +1,21 @@
+public class Objective8Lab3 {
+  public static void main(String[] args) {
+    int counter = 1;
+    for (counter=1; counter <= 20; counter++){
+
+      System.out.print(counter);
+  //         counter = counter++;
+
+            if ( (counter % 2) == 0 ) {
+              System.out.println(" is even ");
+            } else {
+              System.out.println(" is odd ");
+            }
+        }
+    /*
+      Your solution goes here
+    */
+
+  //  System.out.println(counter);
+  }
+}
